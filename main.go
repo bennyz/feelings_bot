6869b19eb1b3501607821c952661562fc84185c8
Matan Shukry <matanshukry@gmail.com>
2018-07-15 20:30:26 +0300

    Changed readmine a bit to be useful Resolve issues #2

--- main.go
+++ main.go
@@ -6,12 +6,29 @@ import (
 	"os"
 	"time"
 
+	"encoding/json"
+	"strings"
+	"io/ioutil"
+	"bytes"
+	"net/http"
+
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/peterhellberg/giphy"
 )
 
+type indicoResponse struct {
+	Results map[string]interface{}
+}
+
+var indicoKey = os.Getenv("INDICO_KEY")
+
+var netClient = &http.Client{
+	Timeout: time.Second * 10,
+}
+
 func main() {
 	botToken := os.Getenv("TELEGRAM_TOKEN")
+	log.Printf("Key: [%s]", botToken)
 	bot, err := tgbotapi.NewBotAPI(botToken)
 	if err != nil {
 		log.Panic(err)
@@ -37,12 +54,11 @@ func main() {
 			bot.Send(getReplyMessage(chatID,
 				message,
 				"How are you feeling?"))
-		} else if message == "Sad" || message == "Happy" {
+		} else {
 			bot.Send(getReplyMessage(chatID,
 				message,
 				getGIF(message)))
 		}
-
 	}
 }
 
@@ -60,6 +76,8 @@ func getReplyMessage(chatID int64, message string, reply string) tgbotapi.Messag
 }
 
 func getGIF(text string) string {
+	text = resolveEmotion(text)
+
 	g := giphy.DefaultClient
 	s, _ := g.Search([]string{text})
 
@@ -67,4 +85,74 @@ func getGIF(text string) string {
 	r := rand.New(seed)
 	imageIndex := r.Intn(len(s.Data))
 	return s.Data[imageIndex].URL
+}
+
+func resolveEmotion(text string) string {
+	textLower := strings.ToLower(text)
+	if (textLower == "sad" || textLower == "happy") {
+		return text;
+	}
+
+	return fetchEmotion(text)
+}
+
+func fetchEmotion(text string) string {
+	url := "https://apiv2.indico.io/emotion"
+	var jsonStr = []byte(`{"data": "` + text + `", "top_n": 1}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-ApiKey", indicoKey)
+	req.Header.Set("client-lib", "golang")
+	req.Header.Set("version-number", "0.9.0")
+	req.Header.Set("Accept", "text/plain")
+
+	resp, err := netClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	log.Println("response status: ", resp.Status)
+	log.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println("response Body:", string(body))
+
+	var jsonResponse indicoResponse
+	// var jsonResponse map[string]interface{}
+	json.Unmarshal(body, &jsonResponse)
+
+	//var results map[string]interface{} = jsonResponse["results"]
+	results := jsonResponse.Results
+
+	key := text
+	for k := range results {
+		key=k
+		break
+	}
+
+	log.Printf("Detected emotion: %s", key)
+	return key
+
+	// 2018/07/15 20:07:29 response Body: {"results": {"anger": 0.1554698348045349, "surprise": 0.1836523711681366, "sadness": 0.28770002722740173, "fear": 0.19637306034564972, "joy": 0.17680470645427704}}
+	// body
+
+	/*
+  return request(options).then(function (results) {
+    var res = results[0];
+    var body = results[1];
+    var warning = res.headers['X-Warning'];
+
+    if (warning) {
+      console.warn(warning);
+    }
+    if (res.statusCode !== 200) {
+      return new Error(JSON.parse(body).error)
+    }
+    results = JSON.parse(body).results;
+    results = postprocess(results, name);
+
+    return results
+  });
+
+  */
 }
